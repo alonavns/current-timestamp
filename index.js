61c5388f884cef8004dab9703e55840bf7161953
alonavns <alona.vns@outlook.com>
2021-01-23 18:06:00 +0200

    cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const PORT = process.env.PORT || 5000
 
 express()
+  .use(cors())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
